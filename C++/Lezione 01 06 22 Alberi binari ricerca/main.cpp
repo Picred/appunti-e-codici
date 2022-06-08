577227c2573ea6bf2c393e57bc1d9bb594c8d533
Picred <andry4you@hotmail.it>
2022-06-08 17:11:46 +0200

    Updated main up to 08/06/2022

--- C++/Lezione 01 06 22 Alberi binari ricerca/main.cpp
+++ C++/Lezione 01 06 22 Alberi binari ricerca/main.cpp
@@ -3,25 +3,50 @@ using namespace std;
 
 int main()
 {
-    BST<int> bst;
+    BST<int> bst;   
 
-    bst.insert(10);
-    bst.insert(4);
-    bst.insert(6);
-    bst.insert(8);
-    bst.insert(2);
-    bst.insert(5);
-    bst.insert(9);
-    bst.insert(3);
+    // bst.max();
+
+    try{
+        bst.max();
+    }
+    catch (const char* ex){
+        cout << ex << endl;
+    }
+    bst.insert(40);
+    bst.insert(25);
+    bst.insert(21);
+    bst.insert(18);
+    bst.insert(57);
+    bst.insert(36);
+    bst.insert(46);
+    bst.insert(32);
+    bst.insert(78);
+    bst.insert(52);
+    bst.insert(11);
+    bst.insert(44);
+
 
     bst.inOrder(); //visita in ordine dell'albero
 
-    cout << "bst.search(9)" << (bst.search(9) ? "found" : "not found") << endl;
+    cout << "bst.search(11) = " << (bst.search(11) ? "found" : "not found") << endl;
 
-    cout << "bst.search(20)" << (bst.search(20) ? "found" : "not found") << endl;
+    cout << "bst.search(20)= " << (bst.search(20) ? "found" : "not found") << endl << endl;
 
     cout << "Min: " << *(bst.min()) << endl;
-    cout << "Max: " << *(bst.max()) << endl;
+    cout << "Max: " << *(bst.max()) << endl << endl;
 
     cout << "Successore di root: " << *(bst.successor(bst.getRoot()));
+
+    cout << "\n\n";
+    BSTNode<int>* cancellato1 = bst.remove(11);
+    cout << "Rimozione 11" << endl;
+    bst.inOrder();
+    
+    BSTNode<int>* cancellato2 = bst.remove(21);
+    cout << "Rimozione 21" << endl;
+    bst.inOrder();
+    BSTNode<int>* cancellato3 = bst.remove(40);
+    cout << "Rimozione 40" << endl;
+    bst.inOrder();
 }
