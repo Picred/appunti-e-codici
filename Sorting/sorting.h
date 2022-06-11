7f1f6e92454039f4b1bbf6bb1526120f6b1da80f
Picred <andry4you@hotmail.it>
2022-06-11 16:17:40 +0200

    Creato namespace sorting e aggiunto main per i test

---
+++ Sorting/sorting.h
@@ -0,0 +1,68 @@
+#ifndef SORTING_H
+#define SORTING_H
+
+#define dim 10
+
+namespace sorting{
+
+    void swap(int& x, int& y){
+        int temp=x;
+        x=y;
+        y=temp;
+    }
+
+    bool dicotomicaRicorsiva(int vett[dim], int key, int start, int end){
+    if(start>end){
+        return false;
+    }
+
+    int m = (end+start)/2;
+
+    if(vett[m]==key){
+        return true;
+    }
+
+    if(key<vett[m]){
+        return dicotomicaRicorsiva(vett,key,start,m-1);
+    }
+
+    if(key>vett[m]){
+        return dicotomicaRicorsiva(vett,key,m+1,end);
+    }
+
+    return false;
+}
+
+    bool dicotomicaIterativa(int vett[], int size, int key){
+        int start=0;
+        int end=size-1;
+        int midpoint;
+        bool found=false;
+
+        while(!found && start<=end){
+            midpoint= (start+end)/2;
+
+            if(vett[midpoint]==key)
+                found=true;
+
+            if(key<vett[midpoint])
+                end=midpoint-1;
+            
+            if(key>vett[midpoint])
+                start=midpoint+1;
+        }
+
+        return found;
+    }
+
+    void ordinamentoPerScambio(int vett[], int size){
+        for(int i=0; i<size-1;i++){
+            for(int j=i+1;j<size;j++){
+                if(vett[j]<vett[i])
+                    swap(vett[i],vett[j]);
+            }
+        }
+    }
+
+}
+#endif
---
+++ Sorting/sorting_test.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include<cstdlib>
+#include "sorting.h"
+
+using namespace std;
+using namespace sorting;
+
+int main(){
+    int vett[dim];
+    
+    cout << "----------RICERCA DICOTOMICA----------\n";
+
+    cout << "Vettore[" ;
+    for(int i=0; i<dim; i++){
+        vett[i] = i;
+        cout  << " " << vett[i] << " ";
+    }
+    cout << "]" << endl;
+
+    
+    cout << "Cerchiamo il 7 nel vettore(ricorsione)..." << endl; 
+    cout << (dicotomicaRicorsiva(vett,7,0,dim-1) ? "Trovato" : "Non trovato") << endl; //true se lo trova, false se non lo trova
+    
+    cout << "Cerchiamo l'8 nel vettore (iterazione)..."<< endl;
+    cout << (dicotomicaIterativa(vett,dim,8) ? "Trovato" : "Non trovato");
+
+
+/////////////////////////////////////////////////////////////////////////
+    cout << "\n\n----------ORDINAMENTO PER SCAMBIO----------\n";
+    
+
+    cout << "Vettore non ordinato[ " ;
+    for(int i=0; i<dim; i++){
+        vett[i] = dim-i;
+        cout  << vett[i] << " ";
+    } 
+    cout << "]" << endl << endl;
+
+    cout << "Ordiniamolo.."<< endl;
+    ordinamentoPerScambio(vett,dim);
+
+    cout << "Vettore ordinato[ " ;
+    for(int i=0; i<dim; i++){
+        cout  << vett[i] << " ";
+    } 
+    cout << "]";
+}
