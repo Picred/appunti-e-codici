16e35c45e70d8a5e2ef63a3b53b3f4adced5cc46
Picred <andry4you@hotmail.it>
2022-06-13 22:15:17 +0200

    Implementato test di tutti i metodi

--- List/main_list.cpp
+++ List/main_list.cpp
@@ -1,25 +1,45 @@
 #include "list.h"
 
-int main(){
+int main() {
     List<int> list;
 
-    cout << list << endl << "inserisco in ordine il 10" << endl;
+    cout << "----------Inserisco in testa 50 40 30 20 10 0 e stampo----------" << endl;
 
-    list.insertCrescente(10);
-    list.insertCrescente(17);
-    list.insertCrescente(48);
+    list.insertHead(50);
+    list.insertHead(40);
+    list.insertHead(30);
+    list.insertHead(20);
+    list.insertHead(10);
+    list.insertHead(0);
 
-    // cout << list;
+    cout << list << endl;
+
+    system("pause");
+
+    cout << "----------Inserisco in coda 60 70 e stampo----------" << endl;
+
+    list.insertTail(60);
+    list.insertTail(70);
+
+    cout << list << endl;
+    system("pause");
+    system("cls");
+    cout << "----------Inserisco in modo crescente 37 e 8 in modo decrescente stampo----------" << endl;
+
+    list.insertCrescente(37);
+    list.insertDecrescente(8);
+
+        cout << list << endl;
+    system("pause");
+    system("cls");
+    cout << "----------Elimino due teste,una coda e il nodo 40 e stampo----------" << endl;
 
     list.removeHead();
-
-    // cout << endl << endl<< list << endl;
-
-    list.insertTail(77);
-    list.insertHead(47);
-
-    cout << "\n\n" << list << endl;
-
+    list.removeHead();
     list.removeTail();
+    list.remove(40);
+
     cout << list;
+    system("pause");
+    system("cls");
 }
