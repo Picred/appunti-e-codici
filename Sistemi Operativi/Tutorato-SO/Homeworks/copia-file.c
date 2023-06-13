fbad14e3fd01554287a63288e5ecb6ca50e591a4
Andrei <102211437+Picred@users.noreply.github.com>
2023-06-13 09:13:59 +0200

    Update copia-file.c

--- Sistemi Operativi/Tutorato-SO/Homeworks/copia-file.c
+++ Sistemi Operativi/Tutorato-SO/Homeworks/copia-file.c
@@ -18,9 +18,9 @@ int copy_file(const char* src,char dst[]){
     strcat(dst_path, "/");
     strcat(dst_path, src);
 
-    FILE* source_file = fopen(src, "rb");
+    FILE* source_file = fopen(src, "r");
     // FILE* dst_file = fopen(dst_path, "w");
-    FILE* dst_file = fopen(dst_path, "wb");
+    FILE* dst_file = fopen(dst_path, "w");
 
     if(source_file == NULL) printf("\nok source\n");
     if(dst_file == NULL) printf("\nok destination     %s\n", dst_path);
